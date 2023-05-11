d502dafcb14dcd5aefcf5d3e74bbfdc2bf7c65c3
Zachary Lord <lordz@go.stockton.edu>
2023-05-10 23:33:16 -0400

    Errur 404

--- newbeg.py
+++ newbeg.py
@@ -1,26 +1,31 @@
-import requests
+import requests as ses
 from bs4 import BeautifulSoup
 
+ 
 # Ask for the Hololive member's name or generation
 search_term = input("What Hololive member or generation do you want to search for? ")
 
+#Q:buddy c what's the syntax for the request_html library?
+#Q: thanks buddy c
+#A
 # Set the search URL
-url = f"https://hololive.hololivepro.com/en/talents?gp=hololive&search={search_term}"
-
+url = f"https://hololive.hololivepro.com/en/talents/{search_term}"
+reponse = ses.get(url)
+print(reponse)
 # Retrieve the search page and parse it using BeautifulSoup
-page = requests.get(url).text
+page = ses.get(url).text
 doc = BeautifulSoup(page, "html.parser")
-
+talent_links = []
 # Find all search results that match the search term
-items = doc.find_all("div", {"class": "talent"})
+talent_list = doc.find_all("ul", {"class": "talent-list clearfix"})
  #Find the total number of search results
-total_results = int(doc.find("p", {"class": "talents-count"}).text.strip().split()[0])
+#pages = int(str(talent_list).split("/")[-2].split(">")[-1][:-1])
 
 # Calculate the number of pages based on the total number of search results (10 search results per page)
-pages = (total_results // 10) + (1 if total_results % 10 != 0 else 0)
 
 # Dictionary to store the search results
 members_found = {}
+# print(page)
 
 # Loop through all search results
 # for item in items:
@@ -44,25 +49,37 @@ members_found = {}
 #     print(member[1]['link'])
 #     print("-------------------------------")
 
-for page in range(1, pages + 1):
+for page in range(1, + 1):
+    
     url = f"https://hololive.hololivepro.com/en/talents?gp=hololive&page={page}"
-    page = requests.get(url).text
+    page = ses.get(url).text
     doc = BeautifulSoup(page, "html.parser")
 
-    talents = doc.find_all("div", {"class": "talent-item"})
+    talents = doc.find_all("ul", {"class": "talent-list clearfix"})
+items_found = {}
+for talent in members_found:
+    try:
+        pagef = ses.urlopen(talent)
+    except ses.HTTPError:
+        continue
+    url = f"https://hololive.hololivepro.com{search_term}"
+    page = ses.get(url).text
+    doc = BeautifulSoup(page, "html.parser")
+    
 
-    for talent in talents:
-        name = talent.find("h4").text.strip()
-        generation = talent.find("h5").text.strip()
-        link = "https://www.youtube.com" + talent.find("a", href=True)["href"]
-        
-        if search_term.lower() in name.lower():
-            members_found[name] = {"generation": generation, "link": link}
+    talent_name = doc.find("div", {"class": "talent-list clearfix"}).text.strip()
+    print(talent_name)
+    talent_generation = doc.find("ul", {"class": "clearfix"}).text.strip()
+
+
+    items_found[talent_name] = {"generation": talent_generation}
+# print(items_found)
 
 sorted_items = sorted(members_found.items(), key=lambda x: x[1]['generation'])
 
 for item in sorted_items:
     print(item[0])
     print(item[1]['generation'])
-    print(item[1]['link'])
+    print(item[0] + item[1]['generation'])
     print("-------------------------------")
+
